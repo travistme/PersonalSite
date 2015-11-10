2ad25761e835fb7e24de8ae691cf7ad5f011a4b3
Travis Elkins <travistme@gmail.com>
2015-11-10 15:44:20 -0500

    Add bundle for CSS and JQuery

---
+++ TElkins.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Web;
+using System.Web.Optimization;
+
+namespace TElkins.Web.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/clientfeaturescripts").Include(
+                "~/Scripts/jquery-{version}.js"));
+        }
+    }
+}
