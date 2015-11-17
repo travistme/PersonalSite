7f8f40b6307fb1f673bcc74ae9397617f49daef4
Travis Elkins <travistme@gmail.com>
2015-11-17 16:43:15 -0500

    Remove unused images, separate css and bundle them

--- TElkins.Web/App_Start/BundleConfig.cs
+++ TElkins.Web/App_Start/BundleConfig.cs
@@ -6,11 +6,34 @@ namespace TElkins.Web
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                "~/Content/*.css"));
+//#if !Debug
+//            bundles.UseCdn = true;
+//            BundleTable.EnableOptimizations = true;
+//#endif
 
-            bundles.Add(new ScriptBundle("~/bundles/clientfeaturescripts").Include(
-                "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new StyleBundle("~/bundles/sitecss")
+                .Include("~/Content/styles.css")
+                .Include("~/Content/homestyles.css")
+                .Include("~/Content/aboutstyles.css")
+                .Include("~/Content/contactstyles.css")
+                );
+
+            bundles.Add(new StyleBundle("~/bundles/bootstrapcss", "//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css")
+                .Include("~/Content/bootstrap.css")
+                );
+
+            bundles.Add(new StyleBundle("~/bundles/bootstrap-themecss", "//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css")
+                .Include("~/Content/bootstrap-theme.css")
+                );
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryjs")
+                .Include("~/Scripts/jquery-{version}.js")
+                );
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrapscripts", "//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js")
+                .Include("~/Scripts/bootstrap.js")
+                );
+
         }
     }
 }
